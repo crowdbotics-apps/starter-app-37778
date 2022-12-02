33dd5cefc3ec4c90ebbfc5ea9919049e4dce88d9
steve.motola@infobeans.com <steve.motola@infobeans.com>
2022-12-02 10:48:21 -0800

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "starter_app_37778",
   url: "https://starter_app_37778.botics.co",
   api: "https://starter_app_37778.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
